008653b3d0c0c762eede128181d3f7a9f73b8a44
Lucas Bastos <lucsbasto@gmail.com>
2025-05-20 14:10:08 -0300

    fix: update Fastify initialization to use a server instance and streamline bootstrap process

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import * as dotenv from 'dotenv';
+import fastify from 'fastify';
 import { setupGlobalPipes } from './common/handlers/global-pipes.handler';
 import { setupSecurity } from './common/handlers/secutiry.handler';
 import { setupSwagger } from './common/handlers/swagger.handler';
@@ -8,13 +10,15 @@ import { ApiResponseInterceptor } from './common/interceptors/api-response.inter
 import { LoggerService } from './common/logger/logger.service';
 import { MainModule } from './main.module';
 
-// Fastify instance não é usada diretamente mais
-let app: NestFastifyApplication;
+dotenv.config();
+const server = fastify();
 
 async function bootstrap() {
   const appLogger = new LoggerService();
 
-  app = await NestFactory.create<NestFastifyApplication>(MainModule, new FastifyAdapter(), { logger: appLogger });
+  const app = await NestFactory.create<NestFastifyApplication>(MainModule, new FastifyAdapter(server), {
+    logger: appLogger,
+  });
 
   app.useGlobalInterceptors(new ApiResponseInterceptor());
 
@@ -24,23 +28,13 @@ async function bootstrap() {
 
   setupGlobalPipes(app);
   setupSwagger(app);
+
   setupSecurity(app, configService);
 
   app.setGlobalPrefix('api');
 
-  await app.init(); // ⚠️ Não use app.listen() no Vercel
-
-  logger.info(`NestJS app initialized on port ${port}`);
+  await app.listen(port ?? 3000).then(() => {
+    logger.info(`Http server listening at port: ${port}`);
+  });
 }
-
 bootstrap();
-
-// ⚠️ Esta é a parte fundamental para o Vercel funcionar
-module.exports = async (req: any, res: any) => {
-  if (!app) {
-    await bootstrap();
-  }
-
-  await app.init(); // garante que o app está pronto
-  app.getHttpAdapter().getHttpServer()(req, res); // proxy do handler fastify
-};
