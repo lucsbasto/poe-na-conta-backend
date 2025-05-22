f38fbc60f9d57f024c1210e6b3081c2c445dd486
Lucas Bastos <lucsbasto@gmail.com>
2025-05-22 11:02:28 -0300

    fix: update bcrypt import to use bcryptjs for compatibility

--- src/domain/common/helpers/password-management.ts
+++ src/domain/common/helpers/password-management.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 export const passwordCompare = async (input: string, exists: string): Promise<boolean> => {
   if (!input || !exists) {
